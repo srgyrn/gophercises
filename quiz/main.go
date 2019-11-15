a37d4f867b3173a2a0a8d21fefca9994f248091a
Sera Geyran <sgeyran@gmail.com>
2019-11-15 13:54:05 +0300

    ask questions and take answers for score calculations

--- quiz/main.go
+++ quiz/main.go
@@ -6,17 +6,55 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"sync"
+	"strings"
+)
+
+var (
+	score int
+	wrongAnswers int
+	wg sync.WaitGroup
+	buf *bufio.Reader
 )
 
 func Start() {
 	workingDir, _ := os.Getwd()
 	csvFile, err := os.Open(workingDir+"/quiz/problems.csv")
+	wg.Add(1)
+	buf = bufio.NewReader(os.Stdin)
 
 	if !errors.Is(err, nil) {
 		fmt.Println(err, workingDir)
 	}
+	questions := csv.NewReader(bufio.NewReader(csvFile))
 
-	data := csv.NewReader(bufio.NewReader(csvFile))
+	go askQuestion(questions)
+	wg.Wait()
 
-	fmt.Print(data.ReadAll())
+	fmt.Printf("Your score: %d. Questions answered wrong: %d", score, wrongAnswers)
+}
+
+func askQuestion(questions *csv.Reader) {
+	defer wg.Done()
+
+	for {
+		question, err := questions.Read()
+
+		if !errors.Is(err, nil) {
+			break
+		}
+
+		fmt.Printf("What %s is? ", question[0])
+		answer, _ := buf.ReadString('\n')
+
+		if strings.TrimSpace(answer) == strings.TrimSpace(question[1]) {
+			score++
+		} else {
+			wrongAnswers++
+		}
+	}
+}
+
+func timer() {
+
 }
