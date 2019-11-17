ae09beb9c726e2eb64ed27966ef7c098ff30a018
Sera Geyran <sgeyran@gmail.com>
2019-11-17 22:09:17 +0300

    Squashed commit of the following:
    commit 80565cfe478ee3ab6877136c5e3b97f126ae52b3
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Sun Nov 17 22:08:53 2019 +0300
    
        Named loop, added break
    
    commit 0cf797aae06375a4b93e5ecfc177e169d4cea341
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Sun Nov 17 22:06:01 2019 +0300
    
        timer added
    
    commit adb8cee8d22bf8fe19df4d8eedf5d35cce1be874
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Sun Nov 17 18:48:14 2019 +0300
    
        Helper function for printing errors and exiting. Added csv filename option taken from console input instead of giving a hardcoded value. Moved global variables to inside the function they are used in
    
    commit 40f4c701133f8014d75de9541eb21c7d4d51fd0e
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Sun Nov 17 17:06:42 2019 +0300
    
        Added console options with flag package
    
    commit f1a6c48f2f4cc544ee2a7c6bd9eed90acff79c01
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Sun Nov 17 00:16:42 2019 +0300
    
        Updated code to be able to give total number of questions in the feedback
    
    commit 0dabb08dc1316ea96efd408ded12918238277b62
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Fri Nov 15 14:12:34 2019 +0300
    
        changed function call to anonymous one
    
    commit 775c3df414b19b79ea5b53df15538ca829cb14a4
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Fri Nov 15 13:54:54 2019 +0300
    
        import order refactored
    
    commit a37d4f867b3173a2a0a8d21fefca9994f248091a
    Author: Sera Geyran <sgeyran@gmail.com>
    Date:   Fri Nov 15 13:54:05 2019 +0300
    
        ask questions and take answers for score calculations

--- quiz/main.go
+++ quiz/main.go
@@ -4,19 +4,61 @@ import (
 	"bufio"
 	"encoding/csv"
 	"errors"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func Start() {
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	limit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
+
 	workingDir, _ := os.Getwd()
-	csvFile, err := os.Open(workingDir+"/quiz/problems.csv")
+	csvFile, err := os.Open(workingDir + "/quiz/" + *csvFileName)
 
 	if !errors.Is(err, nil) {
-		fmt.Println(err, workingDir)
+		exit(fmt.Sprintf("%s not found in %s/quiz/\nError: %v", *csvFileName, workingDir, err))
 	}
 
-	data := csv.NewReader(bufio.NewReader(csvFile))
+	questions, err := csv.NewReader(bufio.NewReader(csvFile)).ReadAll()
 
-	fmt.Print(data.ReadAll())
+	if !errors.Is(err, nil) {
+		exit("cannot read questions from file")
+	}
+
+	timer := time.NewTimer(time.Duration(*limit) * time.Second)
+
+	var score int
+
+problemLoop:
+	for _, question := range questions {
+		fmt.Printf("What %s is? ", question[0])
+		answerCh := make(chan string)
+
+		go func(){
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- strings.TrimSpace(answer)
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Print("\nTime's up!\n")
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == strings.TrimSpace(question[1]) {
+				score++
+			}
+		}
+	}
+
+	fmt.Printf("Your scored %d out of %d.", score, len(questions))
+}
+
+func exit(message string) {
+	fmt.Print(message)
+	os.Exit(1)
 }
