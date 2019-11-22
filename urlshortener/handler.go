755f27d5258d5ea86e053340192447891b391415
Sera Geyran <sgeyran@gmail.com>
2019-11-22 13:56:43 +0300

    Added json handler and updated the code to read routes from file

--- urlshortener/handler.go
+++ urlshortener/handler.go
@@ -1,14 +1,15 @@
 package urlshortener
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/go-yaml/yaml"
 	"net/http"
 )
 
-type yamlLine struct {
-	Path string `yaml:"path"`
-	Url string `yamlL:"url"`
+type routeData struct {
+	Path string `yaml:"path" json:"path"`
+	Url string `yaml:"url" json:"url"`
 }
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -46,7 +47,7 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	var routes []yamlLine
+	var routes []routeData
 	err := yaml.Unmarshal(yml, &routes)
 
 	if !errors.Is(err, nil) {
@@ -56,9 +57,36 @@ func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	return MapHandler(buildMap(routes), fallback), nil
 }
 
+// JSONHandler will parse the provided JSON and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// JSON is expected to be in the format:
+//
+//     "path": "/some-path"
+//      "url": "https://www.some-url.com/demo"
+//
+// The only errors that can be returned all related to having
+// invalid JSON data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func JSONHandler(jsn []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var routes []routeData
+	err := json.Unmarshal(jsn, &routes)
 
-// buildMap takes a slice of yamlLine and converts it to a map
-func buildMap(routes []yamlLine) map[string]string {
+	if !errors.Is(err, nil) {
+		return nil, err
+	}
+
+	return MapHandler(buildMap(routes), fallback), nil
+}
+
+
+// buildMap takes a slice of routeData and converts it to a map
+func buildMap(routes []routeData) map[string]string {
 	routeMap := make(map[string]string)
 
 	for _, line := range routes {
--- urlshortener/main/main.go
+++ urlshortener/main/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/srgyrn/gophercises/urlshortener"
 )
 
 func main() {
 	mux := defaultMux()
-
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
@@ -17,20 +21,16 @@ func main() {
 	}
 	mapHandler := urlshortener.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshortener.YAMLHandler([]byte(yaml), mapHandler)
+	extension := flag.String("file", "json", "File extension (yaml, json)")
+	flag.Parse()
+
+
+	handler, err := getHandler(*extension, mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", handler)
 }
 
 func defaultMux() *http.ServeMux {
@@ -41,4 +41,36 @@ func defaultMux() *http.ServeMux {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
+}
+
+func getRoutesFromFile(extension string) []byte {
+	filePath := "./routes/routing."+extension
+	file, err := os.Open(filePath)
+	checkErrorAndExit(err, "file not found | " + filePath)
+
+	output, err := ioutil.ReadAll(bufio.NewReader(file))
+	checkErrorAndExit(err, "failed at reading file")
+
+	return output
+}
+
+func checkErrorAndExit(err error, message string) {
+	if !errors.Is(err, nil) {
+		fmt.Println("Error: "+message)
+		os.Exit(1)
+	}
+}
+
+func getHandler(fileType string, fallback http.HandlerFunc) (http.HandlerFunc, error) {
+	routes := getRoutesFromFile(fileType)
+	switch fileType {
+	case "yaml":
+		// Build the YAMLHandler using the mapHandler as the
+		// fallback
+		return urlshortener.YAMLHandler(routes, fallback)
+	case "json":
+		return urlshortener.JSONHandler(routes, fallback)
+	default:
+		return nil, errors.New("handler type not found")
+	}
 }
